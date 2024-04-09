f7d12060e358fb71dd9c0f2a5d3b652759b8e957
Lubarskiy Konstantin <lubarskiy.k@gmail.com>
2024-04-09 15:25:35 +0300

    project start
    add fonts
    add partials
    add css for  partials
    add gitignor

---
+++ vite.config.js
@@ -0,0 +1,37 @@
+import { defineConfig } from 'vite';
+import glob from 'glob';
+import injectHTML from 'vite-plugin-html-inject';
+import FullReload from 'vite-plugin-full-reload';
+import ViteWebfontDownload from 'vite-plugin-webfont-dl';
+
+export default defineConfig(({ command }) => {
+  return {
+    define: {
+      [command === 'serve' ? 'global' : '_global']: {},
+    },
+    root: 'src',
+    build: {
+      sourcemap: true,
+
+      rollupOptions: {
+        input: glob.sync('./src/*.html'),
+        output: {
+          manualChunks(id) {
+            if (id.includes('node_modules')) {
+              return 'vendor';
+            }
+          },
+          entryFileNames: 'commonHelpers.js',
+        },
+      },
+      outDir: '../dist',
+    },
+    plugins: [
+      injectHTML(),
+      FullReload(['./src/**/**.html']),
+      ViteWebfontDownload([
+        'https://fonts.googleapis.com/css2?family=Inter+Tight:wght@500;600;700&display=swap',
+      ]),
+    ],
+  };
+});
