4cf97888c15da3f6708fa45391c3c5abef05ff5e
Olga <sinkolga2233@gmail.com>
2024-04-11 13:41:00 +0200

    Section
    Секція готова

--- src/js/aos.js
+++ src/js/aos.js
@@ -5,7 +5,7 @@ AOS.init({
   mirror: true,
   easing: 'ease-out-cubic',
   anchorPlacement: 'top-bottom"',
-  offset: 100, // offset (in px) from the original trigger point
+  offset: 120, // offset (in px) from the original trigger point
   delay: 50, // values from 0 to 3000, with step 50ms
   duration: 500, // values from 0 to 3000, with step 50ms
 });
