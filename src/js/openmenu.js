02252746d48bebf0e204e6d52705cd042b09a953
Lubarskiy Konstantin <lubarskiy.k@gmail.com>
2024-04-12 15:37:21 +0300

    fix

--- src/js/openmenu.js
+++ src/js/openmenu.js
@@ -2,6 +2,7 @@
   const burger = {
     BurgerBtnOpen: document.querySelector('#header-burger-open'),
     BurgerBtnClose: document.querySelector('#header-burger-close'),
+    modal: document.querySelector('.modal'),
     burger: document.querySelector('.navigation-wrapper'),
     body: document.querySelector('body'),
   };
@@ -9,6 +10,6 @@
   burger.BurgerBtnClose.addEventListener('click', togleBurger);
   function togleBurger() {
     burger.burger.classList.toggle('is-open');
-    
-      }
+    burger.modal.classList.toggle('is-open-modal');
+  }
 })();
