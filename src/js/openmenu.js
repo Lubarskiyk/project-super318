75ff18a81dbd4d0b76530b2ebefb25473a9ade11
Lubarskiy Konstantin <lubarskiy.k@gmail.com>
2024-04-12 16:29:15 +0300

    fix

--- src/js/openmenu.js
+++ src/js/openmenu.js
@@ -3,13 +3,14 @@
     BurgerBtnOpen: document.querySelector('#header-burger-open'),
     BurgerBtnClose: document.querySelector('#header-burger-close'),
     modal: document.querySelector('.modal'),
-    burger: document.querySelector('.navigation-wrapper'),
     body: document.querySelector('body'),
+    burger: document.querySelector('.navigation-wrapper'),
   };
   burger.BurgerBtnOpen.addEventListener('click', togleBurger);
   burger.BurgerBtnClose.addEventListener('click', togleBurger);
   function togleBurger() {
     burger.burger.classList.toggle('is-open');
     burger.modal.classList.toggle('is-open-modal');
+    burger.body.classList.toggle('lock');
   }
 })();
