69ca1787b18f22c5c1d14c9707078dd6e99f8fb1
Lubarskiy Konstantin <lubarskiy.k@gmail.com>
2024-04-13 16:22:08 +0300

    modified:   src/js/swiper.js

--- src/js/swiper.js
+++ src/js/swiper.js
@@ -1,4 +1,17 @@
 const swiper = new Swiper('.mySwiper', {
+  breakpoints: {
+    320: {
+      slidesPerView: 1,
+    },
+
+    768: {
+      slidesPerView: 2,
+    },
+
+    1280: {
+      slidesPerView: 3,
+    },
+  },
   pagination: {
     el: '.swiper-pagination',
   },
