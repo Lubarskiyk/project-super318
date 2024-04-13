bb7550ba7ff7f9e959c7108bca7ccc8338030e5b
Lubarskiy Konstantin <lubarskiy.k@gmail.com>
2024-04-13 16:32:03 +0300

    modified:   src/js/swiper.js

--- src/js/swiper.js
+++ src/js/swiper.js
@@ -1,4 +1,11 @@
+import Swiper from 'swiper';
+import { Navigation, Pagination } from 'swiper/modules';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+
 const swiper = new Swiper('.mySwiper', {
+  modules: [Navigation, Pagination],
   breakpoints: {
     320: {
       slidesPerView: 1,
