8b947cf41f55c3a325be1ac17ab78eab3b536431
Lubarskiy Konstantin <lubarskiy.k@gmail.com>
2024-04-14 20:59:22 +0300

    modified:   src/css/main.css 	modified:   src/js/aos.js

--- src/js/aos.js
+++ src/js/aos.js
@@ -5,7 +5,7 @@ AOS.init({
   mirror: true,
   easing: 'ease-out-cubic',
   anchorPlacement: 'top-bottom"',
-  offset: 120, // offset (in px) from the original trigger point
-  delay: 50, // values from 0 to 3000, with step 50ms
-  duration: 500, // values from 0 to 3000, with step 50ms
+  offset: 120,
+  delay: 50,
+  duration: 500,
 });
